cf3bddd943f12e780d8b991d6e896cd579e24735
Thalles Araújo <thalleshenrique.na@gmail.com>
2018-02-21 17:27:13 -0300

    Commit Inicial

---
+++ src/main/java/com/thalles/easymanager/EasymanagerApiApplication.java
@@ -0,0 +1,12 @@
+package com.thalles.easymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymanagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymanagerApiApplication.class, args);
+	}
+}
