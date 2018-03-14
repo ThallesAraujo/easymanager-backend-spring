d80129cb7ddd9db9687235a2e0bec9b19ea61fd2
ThallesAraujo <thalleshenrique.na@gmail.com>
2018-03-14 15:36:38 -0300

    Implementação da feature de atualização completa

--- src/main/java/com/thalles/easymanager/resource/PessoaResource.java
+++ src/main/java/com/thalles/easymanager/resource/PessoaResource.java
@@ -5,14 +5,17 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,41 +24,51 @@ import org.springframework.web.bind.annotation.RestController;
 import com.thalles.easymanager.event.RecursoCriadoEvent;
 import com.thalles.easymanager.model.Pessoa;
 import com.thalles.easymanager.repository.PessoaRepository;
+import com.thalles.easymanager.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaResource {
-	
+
 	@Autowired
 	private PessoaRepository repositorio; 
-	
+
 	@Autowired
 	private ApplicationEventPublisher publisher;
-	
+
+	@Autowired
+	private PessoaService service;
+
 	@GetMapping
 	public List<Pessoa> listar(){
 		return repositorio.findAll();
 	}
 
-	
+
 	@PostMapping
 	public ResponseEntity<Pessoa> adicionar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse resposta){
 		Pessoa instanciaSalva = repositorio.save(pessoa);
-		
+
 		publisher.publishEvent(new RecursoCriadoEvent(this, resposta, instanciaSalva.getId()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(instanciaSalva);
-	
+
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<?> buscarPeloId(@PathVariable Long id){
 		Pessoa ps = repositorio.findOne(id);
 		return ps==null? ResponseEntity.notFound().build() : ResponseEntity.ok(ps);
 	}
-	
+
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long id) {
 		repositorio.delete(id);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long id, @Valid @RequestBody Pessoa pessoa){
+		Pessoa instanciaSalva = service.atualizar(id, pessoa);
+		return ResponseEntity.ok(instanciaSalva);
 	}
 }
---
+++ src/main/java/com/thalles/easymanager/service/PessoaService.java
@@ -0,0 +1,27 @@
+package com.thalles.easymanager.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.thalles.easymanager.model.Pessoa;
+import com.thalles.easymanager.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+	
+	@Autowired
+	private PessoaRepository repositorio;
+	
+	public Pessoa atualizar(Long id, Pessoa pessoa) {
+		Pessoa instanciaSalva = repositorio.findOne(id);
+		if(instanciaSalva == null) {
+			throw new EmptyResultDataAccessException(1);
+		}else {
+			BeanUtils.copyProperties(pessoa, instanciaSalva, "id");
+			return repositorio.save(instanciaSalva);
+	}
+	}
+
+}
