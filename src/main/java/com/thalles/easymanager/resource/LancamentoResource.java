883f69ea035041976c301ef48df6034cf22e84ef
ThallesAraujo <thalleshenrique.na@gmail.com>
2018-03-23 19:09:56 -0300

    Implementação do cadastro de lançamentos

--- src/main/java/com/thalles/easymanager/resource/LancamentoResource.java
+++ src/main/java/com/thalles/easymanager/resource/LancamentoResource.java
@@ -2,14 +2,21 @@ package com.thalles.easymanager.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.thalles.easymanager.event.RecursoCriadoEvent;
 import com.thalles.easymanager.model.Lancamento;
 import com.thalles.easymanager.repository.LancamentoRepository;
 
@@ -32,6 +39,15 @@ public class LancamentoResource {
 	public ResponseEntity<?> buscarPeloId(@PathVariable Long codigo){
 		Lancamento lancamento = repositorio.findOne(codigo);
 		return lancamento == null? ResponseEntity.notFound().build() : ResponseEntity.ok(lancamento);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> adicionar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse resposta){
+		
+		Lancamento instanciaSalva = repositorio.save(lancamento);
+		publisher.publishEvent(new RecursoCriadoEvent(this, resposta, instanciaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(instanciaSalva);
+		
 	}
 	
 }
