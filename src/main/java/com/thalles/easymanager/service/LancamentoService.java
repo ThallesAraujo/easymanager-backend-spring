ef1c3525126142e08b371d51c945e846f040a4fd
Thalles Araújo <thalleshenrique.na@gmail.com>
2018-06-25 14:05:18 -0300

    Atualização de lancamentos

--- src/main/java/com/thalles/easymanager/service/LancamentoService.java
+++ src/main/java/com/thalles/easymanager/service/LancamentoService.java
@@ -1,5 +1,6 @@
 package com.thalles.easymanager.service;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +27,44 @@ public class LancamentoService {
 		}
 		
 		return lancamentoRepository.save(lancamento);
+		
+	}
+	
+	
+	public Lancamento atualizar (Long codigo, Lancamento lancamento) {
+		Lancamento saved = buscarLancamentoExistente(codigo);
+		
+		if(!lancamento.getPessoa().equals(saved.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+		
+		BeanUtils.copyProperties(saved, lancamento, "codigo");
+		
+		return lancamentoRepository.save(saved);
+		
+	}
+
+
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		Lancamento existente = lancamentoRepository.findOne(codigo);
+		if(existente == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		return existente;
+		
+	}
+
+
+	private void validarPessoa(Lancamento lancamento) {
+		Pessoa pessoa = null;
+		if(lancamento.getPessoa().getId() != null) {
+			pessoa = pessoaRepository.findOne(lancamento.getPessoa().getId());
+		}
+		
+		if(pessoa == null || !(pessoa.isAtivo())) {
+			throw new PessoaInexistenteOuInativaException();
+		}
 		
 	}
 	
