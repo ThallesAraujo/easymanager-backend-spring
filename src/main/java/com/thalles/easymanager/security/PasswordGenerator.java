9ca2cec6b75da7c40ce41f2d2046e42900d21bad
Thalles Araújo <thalleshenrique.na@gmail.com>
2018-10-27 08:45:24 -0300

    Troca de usuários e senhas

---
+++ src/main/java/com/thalles/easymanager/security/PasswordGenerator.java
@@ -0,0 +1,14 @@
+package com.thalles.easymanager.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordGenerator {
+	
+	public static void main(String[] args) {
+		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println(encoder.encode("admin"));
+		
+	}
+
+}
